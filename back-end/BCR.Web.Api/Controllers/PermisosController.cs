78a2fc61a0215c05dc3c4e71cd90286a5ff3c39b
Martin Andres Valentini <martin.valentini@neoris.com>
2019-10-23 18:23:26 +0000

    16 Implementar Authorize - Token
    git-tfs-id: [http://arrosvmtfs08:8080/tfs/DefaultCollection]$/poc/src;C11

--- back-end/BCR.Web.Api/Controllers/PermisosController.cs
+++ back-end/BCR.Web.Api/Controllers/PermisosController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BCR.Business.Domain.Queries.Seguridad;
+﻿using BCR.Business.Domain.Queries.Seguridad;
 using BCR.Crosscutting.Exceptions;
 using BCR.Service.Infrastructure;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace BCR.Web.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PermisosController : ControllerBase
